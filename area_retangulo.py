58c1bfd0814825cae63a3af994d8303b43528830
Iasmim <sousaiasmimpereira15@gmail.com>
2026-04-14 21:05:34 -0300

    cálculo da área do retângulo

---
+++ area_retangulo.py
@@ -0,0 +1,5 @@
+base = float(input("Digite o valor da base do retângulo: "))
+altura = float(input("Digite o valor da altura do retângulo:"))
+
+area = base * altura
+print(f"A área do retângulo com base {base} vezes altura {altura} é: {area}")
